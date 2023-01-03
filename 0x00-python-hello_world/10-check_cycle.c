334d1e27309bea7994fad6afad654f82524903ca
Pelugraph <emmanuelamoo765@gmail.com>
2023-01-02 17:16:10 -0800

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *current, *check;
 
-	if (list == NULL || list->next == NULL)<F9>
+	if (list == NULL || list->next == NULL)
 		return (0);
 	current = list;
 	check = current->next;
